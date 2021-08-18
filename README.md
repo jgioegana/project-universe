39a32d081c9f8683805518782ed097ddff7ba92e
jgioegana <jgioegana@gmail.com>
2021-08-18 00:31:38 -0700

    Launch Commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # project-universe
+Create a scrollable 3D dimension that showcases your different projects, in the form of planets.
+
+Disclaimer: This effect is better suited in desktop/laptop viewports. May need a little bit of tweaking when implementing in mobile.
+
+# Demo
+Check the demo: <link>
+
+Check my website: https://gioreyes.me/
+
+# Prerequisites
+Download [jQuery](https://jquery.com/download/).
+
+# How to update
+**Project Array (projArr):** Load your project images here. All images must be the same image size (recommended).
+
+**Project Names (projName):** Input the names of the projects in order of the images in the "projArr" array. The number of project names in this array must match the length of the "projArr" array.
+
+**Project Links (projLink):** This is the array where you put the links of each of your projects. The number of project links in this array must match the length of the "projArr" array.
+
+**Universe Background Color (bgColor):** Set your preferred "universe" background color.
+
+**Universe Background Image (bgImage):** Set your preferred "universe" background image. Must uncomment setting of background-image located in the "init" function.
+
+**Scroll Speed (scrollSpd):** Determines how fast the visitor can travel through the projects. Increase to go faster, decrease to go slower.
+
+**Current Position (currPos):** Determines the visitor's current position in the "universe". This is the basis if the visitor can go further or nearer, based on the values set for min and max.
+
+**Project Distance (distance):** Determines the distance between projects. Increase the value to expand the distance between projects, decrease the value to compress the projects.
+
+**Minimum Distance (min):** Determines the nearness limit. This is the value that "currPos" is based, and the visitor starts in this limit when the page loads.
+
+**Maximum Distance (max):** Determines the limit on how far the visitor can traverse in the "universe".
+
+**Project Planet Positioning (planetPos):** Two dimensional array; contains the top and left positioning of each planet projects. The syntax is "top","left". The number of project positions in this array must match the length of the "projArr" array.
--- scripts.js
+++ scripts.js
@@ -1,19 +1,39 @@
 var planet;
-var projArr = ['img1.png','img2.png','img3.png','img4.png','img5.png','img6.png','img7.png','img8.png','img9.png','img10.png'];
-var projName = ['Project1','Project2','Project3','Project4','Project5','Project6','Project7','Project8','Project9','Project10']; //must be same size with projArr
+var link;
+var projArr = [
+    'img1.png','img2.png','img3.png',
+    'img4.png','img5.png','img6.png',
+    'img7.png','img8.png','img9.png',
+    'img10.png'
+];
+var projName = [
+    'Alpha','Beta','Charlie',
+    'Delta','Echo','Foxtrot',
+    'Golf','Hotel','India',
+    'Juliett'
+];
+var projLink = [
+    'https://www.gioreyes.me','https://www.gioreyes.me','https://www.gioreyes.me',
+    'https://www.gioreyes.me','https://www.gioreyes.me','https://www.gioreyes.me',
+    'https://www.gioreyes.me','https://www.gioreyes.me','https://www.gioreyes.me',
+    'https://www.gioreyes.me'
+];
+let planetPos = [
+    ["20vh","50vw"],["20vh","20vw"],["50vh","30vw"],
+    ["60vh","60vw"],["20vh","50vw"],["40vh","30vw"],
+    ["20vh","60vw"],["20vh","20vw"],["50vh","30vw"],
+    ["39vh","45vw"]
+];
 let bgColor = "black";
-let bgImage = "bg";
-let scrollSpd = 200; //speed of travel to and from planets projects
-let currPos = -200; //basis for movement limit; if zoom is already at min or max; current position of zoom
-var distance = 800; //distance of each planet projects to each other
-var min = -200; //minimum value of zoom// starts here when loaded
-var max = (min - (distance * (projArr.length-1)))-500; //maximum value of zoom// contact slide pops up here //500 is just an allowance for the last slide to look good
-let planetPos = [["10vw","50vw"],["10vw","20vw"],["25vw","30vw"],["30vw","60vw"],["10vw","50vw"],["20vw","30vw"],["10vw","60vw"],["10vw","20vw"],["25vw","30vw"],["18vw","45vw"]]; //top left; needs to be user managed since its up to them to place the PPS wherever they want
-let planetDepth = []; //will contain the depth of each project planet based on user's inputted number of values in projArr. Updates with a new value (+ or - scroll speed) when user scrolls.
+let bgImage = "media/bg.jpg";
+let scrollSpd = 200;
+var currPos = -200;
+let distance = 800;
+var min = -200;
+var max = (min - (distance * (projArr.length-1)))-500;
+let planetDepth = [];
 
 let universe= document.getElementById("universe");
-let planetCt = projArr.length-1;
-
 var temp = projArr.length-1;
 
 window.onload = function() {
@@ -26,7 +46,7 @@ function init() {
     $("#universe").css('background-color',bgColor);
     /*
     Uncomment below if you want to use image instead.
-    $("#universe").css('background-image', 'url(media/' + bgImage + '.jpg)');
+    $("#universe").css('background-image', 'url(bgImage)');
     $("#universe").css('background-repeat', 'no-repeat');
     $("#universe").css('background-size', '100% 100%');
     */
@@ -41,7 +61,14 @@ function loadPlanets() {
         planet.style.backgroundSize = "100% 100%";
         planet.style.top = planetPos[i][0];
         planet.style.left = planetPos[i][1];
-        planet.innerText = projName[i];
+
+        link = document.createElement('a');
+        link.style.textDecoration = "none";
+        link.style.color = "white";
+        link.href = projLink[i];
+        link.innerText = projName[i];
+        planet.append(link);
+
         planet.style.transform = "matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,0,0," + (min - (distance * i)) + ",1)";
         planetDepth[i] = min - (distance * i);
         planet.style.zIndex = temp--;
@@ -56,13 +83,13 @@ window.addEventListener("wheel", function(e) {
 
 function zoom(dir){
     if(dir == "in"){
-        for(var i = 0; i <= projArr.length-1; i++){ //let all projects hop the speed towards the screen
+        for(var i = 0; i <= projArr.length-1; i++){
             planetDepth[i] += scrollSpd;
             $('.planet').get(i).style.transform = "matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,0,0," + planetDepth[i] + ", 1)";
         }
     }
     else if(dir == "out"){
-        for(var i = 0; i <= projArr.length-1; i++){ //let all projects hop the speed towards the screen
+        for(var i = 0; i <= projArr.length-1; i++){
             planetDepth[i] -= scrollSpd;
             $('.planet').get(i).style.transform = "matrix3d(1,0,0.00,0,0.00,1,0.00,0,0,0,1,0,0,0," + planetDepth[i] + ", 1)";
         }
